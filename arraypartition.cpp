e27ab6d4487fbfeb457d63924ebfb9bf530d9a25
perween <s9perween@gmail.com>
2022-10-11 12:34:15 +0530

    561. Array Partition

---
+++ arraypartition.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int maxsum = 0;
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<nums.size(); i += 2)
+        {
+            maxsum = maxsum + nums[i];
+        }
+        return maxsum;
+        
+    }
+};
